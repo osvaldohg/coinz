4bd665d0bd7b237ea408a9db3f0fe13bb053dc20
osvaldohg <osvaldohg@gmail.com>
2018-02-26 15:42:08 +0000

    added some comments for code maintenance and removed some debug lines

--- code/coinz.py
+++ code/coinz.py
@@ -1,33 +1,34 @@
 import bitso
-import csv
 import json
 from datetime import datetime
 import tweepy
 
 
 def notify(message,coin,value,timestamp):
-	print "lol"
 	tweet_api.update_status(message+" "+coin+" "+value+" "+timestamp)
 	
 api= bitso.Api()
 
+#Loading App configuration file
 configFileName="../config/config.json"
 config_data=open(configFileName,"r")
-
 data = json.load(config_data)
 config_data.close()
 
 date=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+#Loading keys to for tweet updates
 tweetFileName="../config/twitterKeys.json"
 tweet_data=open(tweetFileName,"r")
 tweet= json.load(tweet_data)
 tweet_data.close()
-#print tweet["twitter"]["CONSUMER_KEY"]
+
+#handling twitter authentication
 auth = tweepy.OAuthHandler(tweet["twitter"]["CONSUMER_KEY"],tweet["twitter"]["CONSUMER_SECRET"])
 auth.set_access_token(tweet["twitter"]["ACCESS_KEY"], tweet["twitter"]["ACCESS_SECRET"])
 tweet_api = tweepy.API(auth)
 
+#Main
 for ecoin in data:
 	print "Checking "+ecoin
 	tick = api.ticker(data[ecoin]["BitsoTag"])
@@ -41,12 +42,12 @@ for ecoin in data:
 	if current-data[ecoin]["Notify"] >= data[ecoin]["Increment"]:
 		print "<<<Notify Increment>>>"
 		notify("Incremento",ecoin,str(current),date)
-		data[ecoin]["Notify"]+=100
+		data[ecoin]["Notify"]+=data[ecoin]["Increment"]
 		
 	elif current-data[ecoin]["Notify"] <0 :
 		print "<<<Notify Decrement>>>"
 		notify("Decremento",ecoin,str(current),date)
-		data[ecoin]["Notify"]-=100
+		data[ecoin]["Notify"]-=data[ecoin]["Increment"]
 		
 	max=data[ecoin]["Max"]
 	min=data[ecoin]["Min"]
@@ -60,9 +61,10 @@ for ecoin in data:
 	print "current "+str(current)
 	print "Max "+str(data[ecoin]["Max"])
 	print "Min "+str(data[ecoin]["Min"])
-	#print date
-	print "********************************"
 	
+	print "********************************"
+
+#update json config file	
 jsonFile = open(configFileName, "w+")
 json.dump(data,jsonFile,indent=4)
 jsonFile.close()
